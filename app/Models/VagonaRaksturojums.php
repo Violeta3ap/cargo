832c0e482f773e1ee504a47a25a5ed0fb21beeed
Violeta <violetatarasova35@gmail.com>
2026-03-06 18:00:06 +0200

    Update VagonaRaksturojums.php

--- app/Models/VagonaRaksturojums.php
+++ app/Models/VagonaRaksturojums.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\VagonuDati;
 
 class VagonaRaksturojums extends Model
 {
@@ -11,4 +12,13 @@ class VagonaRaksturojums extends Model
     protected $keyType = 'int';
     public $incrementing = true;
     public $timestamps = false;
+
+
+      public function vagonudati()
+    {
+        return $this->belongsTo(VagonuDati::class, 'VagonaID', 'VagonaID');
+    }
+    
+
+    
 }
