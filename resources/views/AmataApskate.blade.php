9f0309202df707fc299637f82e0a1b1fa4416acb
Violeta <violetatarasova35@gmail.com>
2026-03-07 19:23:54 +0000

    tab

--- resources/views/AmataApskate.blade.php
+++ resources/views/AmataApskate.blade.php
@@ -35,7 +35,7 @@
 
 
 
-        
+      </table>  
     </div>
     <a href="/Amati/{{ $amati->AmataID }}/edit"  style="border-radius:8px;  border: 1px solid #59c1cf; 
                 padding: 5px; color: #000000; text-decoration: none; background: linear-gradient(to right, #59c1cf, #ffffff)">Rediģēt</a>
