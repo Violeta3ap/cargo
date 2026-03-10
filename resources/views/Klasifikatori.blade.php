ae78c4d709d6afc34dd1e4ef739eacc117821b63
Violeta <violetatarasova35@gmail.com>
2026-03-10 11:51:02 +0000

    adasda

--- resources/views/Klasifikatori.blade.php
+++ resources/views/Klasifikatori.blade.php
@@ -12,12 +12,43 @@
         </div>
 
 
-    <div class="content">
+
+        div class="navigacija">
+
+
+    @if(Auth::check())
+    <a href="/">Par uzņēmumu</a>
+    <a href="/Klienti">Klienti</a>
+    <a href="/Noma">Noma</a>
+    <a href="/VagonuDati">Nomas papildinājums</a>
+    <a href="/Darbinieki">Darbinieki</a>
+    <a href="/Klasifikatori">Klasifikatori</a>
+
+
+
+     <div style="flex: 1; min-width: 0;">
+        <p style="text-align: justify; margin: 0;">
+            <br>
+    <div style="width: 100%; display: flex; justify-content: center; margin-top: 16px;">
+        <div style="background: #ffffff; padding: 5px 5px; border-radius: 20px; box-shadow: 0 2px 12px rgba(0,0,0,0.08); font-weight: bold; text-align: center;">
+            Klasifikatori
+        </div>
+
+
     <a href="/VagonaRaksturojums">Vagonu raksturojums</a>
     <a href="/Veidi">Vagonu veidi</a>
     <a href="/Kravas">Kravas</a>
     <a href="/Amati">Amati</a>
     </div>
+
+
+
+
+    <a href="/VagonaRaksturojums">Vagonu raksturojums</a>
+    <a href="/Veidi">Vagonu veidi</a>
+    <a href="/Kravas">Kravas</a>
+    <a href="/Amati">Amati</a>
+
 
 
 
