08fa8dc5a45f0727b06e3e9a70e763e11ab622a1
Violeta <violetatarasova35@gmail.com>
2026-03-10 08:50:48 +0000

    paroles kom

--- resources/views/KlientiApskate.blade.php
+++ resources/views/KlientiApskate.blade.php
@@ -14,7 +14,7 @@
             <p class="card-text"><strong>Uzvārds:</strong> {{ $klientis->Uzvards }}</p>
             <!-- <p class="card-text"><strong>Parole:</strong> {{ $klientis->Parole }}</p> -->
 
-            <p class="card-text"><strong>Parole:</strong> {{ str_repeat('•', strlen($klientis->Parole)) }}</p>
+            <p class="card-text"><strong>Parole:</strong> {{ str_repeat('•', strlen($klientis->Parole)) }}</p>   <!--tik punktus, cik simbolu ir parolei-->
             <p class="card-text"><strong>E-pasts:</strong> {{ $klientis->Epasts }}</p>
             <p class="card-text"><strong>Telefona numurs:</strong> {{ $klientis->TelefonaNumurs }}</p>
             <p class="card-text"><strong>Uzņēmuma nosaukums:</strong> {{ $klientis->UznemumaNosaukums }}</p>
