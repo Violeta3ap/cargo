b01d33b8733df70a3828e4b7322f16af2f9bc9b4
Violeta <violetatarasova35@gmail.com>
2026-03-10 19:31:44 +0000

    kolonna

--- resources/views/Klasifikatori.blade.php
+++ resources/views/Klasifikatori.blade.php
@@ -1,16 +1,21 @@
 @extends('layout.app')
 
 @section('content')
-<div style="display: flex">
-<h2>Klasifikatori</h2> 
+
+<div style="display: flex; flex-direction: column;">
+
+<h2>Klasifikatori</h2>
+
 <br><br>
-<nav class="navigacija" style="   background-color: #ffffff;">
-<a href="/Darbinieki/jauns" >Jauns ieraksts</a>
-   <a href="/VagonaRaksturojums">Vagona raksturojums</a>
+
+<nav class="navigacija" style="background-color: #ffffff;">
+    <a href="/Darbinieki/jauns">Jauns ieraksts</a>
+    <a href="/VagonaRaksturojums">Vagona raksturojums</a>
     <a href="/Veidi">Vagonu veidi</a>
     <a href="/Kravas">Krava</a>
     <a href="/Amati">Amati</a>
 </nav>
+
 </div>
 
 @endsection
