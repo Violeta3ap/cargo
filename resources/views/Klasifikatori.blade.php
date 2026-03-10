18743a2df84b0d65f6dc883096cef30782bda672
Violeta <violetatarasova35@gmail.com>
2026-03-10 19:40:21 +0000

    asdsadaqd

--- resources/views/Klasifikatori.blade.php
+++ resources/views/Klasifikatori.blade.php
@@ -5,7 +5,6 @@
 <h2>Klasifikatori</h2>
 
 <nav class="navigacija" style="background-color: #ffffff;">
-    <a href="/Darbinieki/jauns">Jauns ieraksts</a>
     <a href="/VagonaRaksturojums">Vagona raksturojums</a>
     <a href="/Veidi">Vagonu veidi</a>
     <a href="/Kravas">Krava</a>
