cac9813684a4ffb6369e727d92fa2b9d4c56d2cc
Violeta <violetatarasova35@gmail.com>
2026-03-10 19:27:21 +0000

    fvc

--- resources/views/Klasifikatori.blade.php
+++ resources/views/Klasifikatori.blade.php
@@ -1,51 +1,26 @@
-
-
-
-
-
-
-
-
-
-
-<!-- <footer>
-    <div>© 2014–2026 VAS "Latvijas dzelzceļš"</div>
-    <div>Emīlijas Benjamiņas iela 3, Rīga, LV-1547</div>
-    <div>Uzziņas: 8002 1181</div>
-    <div>E-pasts: info@ldz.lv</div>
-</footer> -->
-
-
-@extends ('layout.app')
-
+@extends('layout.app')
 
 @section('content')
-
-
-<div style="width: 100%; display: flex; justify-content: center; align-items: center; margin-top: 16px;">
-    <div style="background: #69c6d3; padding: 32px; border-radius: 12px; box-shadow: 0 2px 12px rgba(0,0,0,0.08); width: 100%; display: flex; gap: 32px; align-items: center;">
-        <div style="flex: 0 0 40%; min-width: 0;">
-            <img src="https://ldzcargo.ldz.lv/sites/cargo.ldz.lv/files/styles/slideshow1/public/LDz-Cargo-kravu-parvadajumi-1_0.png?itok=HYB0_oGT"
-             alt="LDZ CARGO" style="width: 600px; border-radius: 8px;">
-        </div>
-
-
-    <div style="flex: 1; min-width: 0;">
-      
-            <br>
-    <div style="width: 100%; display: flex; justify-content: center; margin-top: 16px;">
-        <div style="background: #ffffff; padding: 5px 5px; border-radius: 20px; box-shadow: 0 2px 12px rgba(0,0,0,0.08); font-weight: bold; text-align: center;">
-            Klasifikatori
-        </div>
-    </div>
-    <a href="/VagonaRaksturojums">Vagona raksturojums</a>
+<div style="display: flex">
+<h2>Klasifikatori</h2> 
+<nav class="navigacija" style="   background-color: #ffffff;">
+<a href="/Darbinieki/jauns" >Jauns ieraksts</a>
+   <a href="/VagonaRaksturojums">Vagona raksturojums</a>
     <a href="/Veidi">Vagonu veidi</a>
     <a href="/Kravas">Krava</a>
     <a href="/Amati">Amati</a>
-        </div>
-    </div>
+</nav>
 </div>
-@endsection('content')
+
+@endsection
+
+
+
+
+
+
+
+
 
 
 
