976e5e67d231958e956f191e3499fbed5fe3eb76
Violeta <violetatarasova35@gmail.com>
2026-03-12 14:05:30 +0000

    fadsaddsdasdasd

--- resources/views/Klienti.blade.php
+++ resources/views/Klienti.blade.php
@@ -58,11 +58,11 @@
                  padding: 5px 10px; color: #000000; text-decoration: none; background-color: #59c1cf; white-space: nowrap;" class="btn btn-sm btn-warning">Rediģēt</a>
 
 
-<a href="{{ url('/Noma/'.$noma->NomasID.'/delete') }}"
+<!-- <a href="{{ url('/Noma/'.$noma->NomasID.'/delete') }}"
    onclick="return confirm('Vai tiešām vēlies dzēst šo ierakstu?');"
    style="border-radius:8px; border:1px solid #59c1cf; padding:5px; color:#000000; text-decoration:none; background: linear-gradient(to right, #59c1cf, #ffffff)">
    Dzēst
-</a>
+</a> -->
 
 
 
