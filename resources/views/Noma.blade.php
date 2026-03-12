68ebf3a3c507cd6bbc4313b248526259d962dbf8
Violeta <violetatarasova35@gmail.com>
2026-03-12 14:10:16 +0000

    asdas

--- resources/views/Noma.blade.php
+++ resources/views/Noma.blade.php
@@ -56,7 +56,7 @@
                     padding: 5px 10px; color: #000000; text-decoration: none; background-color: #59c1cf; white-space: nowrap;" class="btn btn-sm btn-warning">Rediģēt</a>
 
 
-<a href="{{ url('/Noma/'.$noma->NomasID.'/delete') }}"
+<a href="{{/Noma/{{ $item->NomasID }}/delete') }}"
    onclick="return confirm('Vai tiešām vēlies dzēst šo ierakstu?');"
    style="border-radius:8px; border:1px solid #59c1cf; padding:5px; color:#000000; text-decoration:none; background: linear-gradient(to right, #59c1cf, #ffffff)">
    Dzēst
