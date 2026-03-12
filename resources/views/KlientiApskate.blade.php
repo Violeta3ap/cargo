4ecbf9e64cd1a0879c3737844436307142fa21ca
Violeta <violetatarasova35@gmail.com>
2026-03-12 20:39:40 +0000

    atkal

--- resources/views/KlientiApskate.blade.php
+++ resources/views/KlientiApskate.blade.php
@@ -33,7 +33,7 @@
                 padding: 5px; color: #000000; text-decoration: none; background: linear-gradient(to right, #59c1cf, #ffffff)">Dzēst</a> -->
 
 
-                <a href="/Klienti/{{ $item->KlientaID }}/delete"
+                <a href="/Klienti/{{ $klientis->KlientaID }}/delete"
                 onclick="return confirm('Vai tiešām vēlies dzēst šo ierakstu?');"
                 style="border-radius:8px;  border: 1px solid #59c1cf; 
                 padding: 5px; color: #000000; text-decoration: none; background: linear-gradient(to right, #59c1cf, #ffffff)">
