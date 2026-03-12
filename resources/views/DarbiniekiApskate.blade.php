0159fdf4754537ced91a6c20c8d7873555b0f05f
Violeta <violetatarasova35@gmail.com>
2026-03-12 20:53:44 +0000

    darb dz

--- resources/views/DarbiniekiApskate.blade.php
+++ resources/views/DarbiniekiApskate.blade.php
@@ -26,6 +26,15 @@
       <br><br>
     <a href="/Darbinieki/{{ $darbinieki->DarbiniekaID }}/edit"  style="border-radius:8px;  border: 1px solid #59c1cf; 
                 padding: 5px; color: #000000; text-decoration: none; background: linear-gradient(to right, #59c1cf, #ffffff)">Rediģēt</a>
-            <a href="/Darbinieki/{{ $darbinieki->DarbiniekaID }}/delete"  style="border-radius:8px;  border: 1px solid #59c1cf; 
-                padding: 5px; color: #000000; text-decoration: none; background: linear-gradient(to right, #59c1cf, #ffffff)">Dzēst</a>
+
+                                <a href="/Darbinieki/{{ $item->DarbiniekaID }}/delete"
+                onclick="return confirm('Vai tiešām vēlies dzēst šo ierakstu?');"
+                style="border-radius:8px;  border: 1px solid #59c1cf; 
+                padding: 5px; color: #000000; text-decoration: none; background: linear-gradient(to right, #59c1cf, #ffffff)">
+                Dzēst
+                </a>
+
+
+            <!-- <a href="/Darbinieki/{{ $darbinieki->DarbiniekaID }}/delete"  style="border-radius:8px;  border: 1px solid #59c1cf; 
+                padding: 5px; color: #000000; text-decoration: none; background: linear-gradient(to right, #59c1cf, #ffffff)">Dzēst</a> -->
 @endsection
