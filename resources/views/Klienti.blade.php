9d151f703dd63e384b25ceae31964da20c007af0
Violeta <violetatarasova35@gmail.com>
2026-03-16 15:48:27 +0000

    detal lienti darb. nonem

--- resources/views/Klienti.blade.php
+++ resources/views/Klienti.blade.php
@@ -25,13 +25,13 @@
             <th>Klienta ID</th>
             <th>Vārds</th>
             <th>Uzvārds</th>
-            <!-- <th>Parole</th> -->
+            <th>Parole</th>
             <th>E-pasts</th>
             <th>Telefona numurs</th>
             <th>Uzņēmuma nosaukums</th>
             <th>Juridiska adrese</th>
-            <!-- <th>Registrācijas numurs</th>
-            <th>Konta numurs</th> -->
+            <th>Registrācijas numurs</th>
+            <th>Konta numurs</th>
             <th>Darbības</th>
         </tr>
     </thead>
@@ -41,19 +41,19 @@
             <td>{{$item->KlientaID}}</td>
             <td>{{$item->Vards}}</td>
             <td>{{$item->Uzvards}}</td>
-            <!-- <td>{{$item->Parole}}</td> -->
+            <td>{{$item->Parole}}</td>
             <td>{{$item->Epasts}}</td>
             <td>{{$item->TelefonaNumurs}}</td>
             <td>{{$item->UznemumaNosaukums}}</td>
             <td>{{$item->JuridiskaAdrese}}</td>
-            <!-- <td>{{$item->RegistracijasNumurs}}</td>
-            <td>{{$item->KontaNumurs}}</td> -->
+            <td>{{$item->RegistracijasNumurs}}</td>
+            <td>{{$item->KontaNumurs}}</td>
             <td>
 
 
             <div style="display: flex; gap: 5px; flex-wrap: wrap;">
-                <a href="/Klienti/{{ $item->KlientaID }}/details" style="border-radius:8px; border: 1px solid #59c1cf;
-                 padding: 5px 10px; color: #000000; text-decoration: none; background-color: #59c1cf; white-space: nowrap;">Detalizēta</a>
+                <!-- <a href="/Klienti/{{ $item->KlientaID }}/details" style="border-radius:8px; border: 1px solid #59c1cf;
+                 padding: 5px 10px; color: #000000; text-decoration: none; background-color: #59c1cf; white-space: nowrap;">Detalizēta</a> -->
                 <a href="/Klienti/{{ $item->KlientaID }}/edit" style="border-radius:8px; border: 1px solid #59c1cf;
                  padding: 5px 10px; color: #000000; text-decoration: none; background-color: #59c1cf; white-space: nowrap;" class="btn btn-sm btn-warning">Rediģēt</a>
 
