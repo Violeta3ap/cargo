d7562356bbd30d5d7c2de04734f7f0ed1757759e
Violeta <violetatarasova35@gmail.com>
2026-03-19 17:01:56 +0000

    atkal2

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -145,15 +145,14 @@
             <!-- <a href="/Klasifikatori">Klasifikatori</a> -->
             <a href="/Veidi">Vagonu veidi</a>
             <a href="/Kravas">Krāvu veidi</a>
+            <span style="position: absolute; right: 120px; font-size: 13px;">{{ Auth::user()->name }}</span>
             <a href="/logout" style="position: absolute; right: 10px; border-radius:8px;  border: 1px solid #59c1cf; 
             padding: 5px; background:#ffffff; text-decoration: none; color: #000000;">Izlogoties</a>
 
         @else
             <a href="/">Par uzņēmumu</a>
             <a href="/Login" style="position: absolute; right: 10px; border-radius:8px;  border: 1px solid #59c1cf; 
-            padding: 5px; background:#ffffff; text-decoration: none; color: #000000;">Ielogoties</a> 
-
-            {{ Auth::user()->name }} <!-- parāda lietotājvārdu, ja ir pieteicies -->
+            padding: 5px; background:#ffffff; text-decoration: none; color: #000000;">Ielogoties</a>
         @endif
 
     </div>
