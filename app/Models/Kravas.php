530641411aa6c52dd40028bf114bcd6378af948c
Violeta <violetatarasova35@gmail.com>
2026-03-24 09:54:57 +0200

    Define relationship with Veidi model in Kravas
    Added relationship method to link Kravas model with Veidi model.

--- app/Models/Kravas.php
+++ app/Models/Kravas.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Veidi;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,4 +12,11 @@ class Kravas extends Model
     protected $keyType = 'int'; // Primārās atslēgas datu tips ir vesels skaitlis
     public $incrementing = true; // Primārā atslēga automātiski palielinās (auto-increment)
     public $timestamps = false; // Nesaglabāt laika zīmogus created_at un updated_at
+
+       // Saistība ar Veidi tabulu
+   public function veidi()
+   {
+       return $this->belongsTo(Veidi::class, 'VeidaID', 'VeidaID'); // Katrs vagons ir konkrēta veida
+   }
+
 }
