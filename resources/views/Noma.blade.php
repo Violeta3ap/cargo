80dc06c630b643818ef1a2264e9e3b538fdcd53c
Violeta <violetatarasova35@gmail.com>
2026-03-25 07:46:26 +0000

    style meg

--- resources/views/Noma.blade.php
+++ resources/views/Noma.blade.php
@@ -18,7 +18,7 @@
 </div>
 
 <!-- Meklēšanas filtri -->
-<form method="GET" action="/Noma" class="noma-filter-form">
+<form method="GET" action="/Noma" class="noma-filter-form" style="padding: 8px 10px;">
     <input type="text" name="klients" value="{{ $klients }}" placeholder="Klienta uzņēmuma nosaukums">
     <input type="text" name="krava" value="{{ $krava }}" placeholder="Kravas veids">
     <input type="text" name="veidii" value="{{ $veidii }}" placeholder="Vagona nosaukums">
