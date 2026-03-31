32a34788b8c9ee09a061805468a81c83cefb542c
Violeta <violetatarasova35@gmail.com>
2026-03-31 09:46:30 +0300

    Update link text for creating rental application

--- resources/views/Noma.blade.php
+++ resources/views/Noma.blade.php
@@ -36,7 +36,7 @@
     <h2>Noma</h2>
     <nav class="navigacija" style="background-color: #ffffff; padding: 5px 10px;">
         <a href="/Noslogojums">Noslogojums</a>
-        <a href="/Noma/jauns">Izveidot nomu</a>
+        <a href="/Noma/jauns">Izveidot nomas pieteikumu</a>
         <a type="button" onclick="window.print()" title="Printēt dokumentu" class="print-btn"><i class="fas fa-print"></i> Drukāt</a>
     </nav>
 </div>
